a8cafda3cc922e0057919c0db019888e348635bf
alizat <ali_ezzat85@yahoo.com>
2018-11-17 22:47:34 +0200

    prepare project dataset + backup of fallen ideas for project tasks

--- ideas.R
+++ ideas.R
@@ -1,5 +1,7 @@
 lapply(c("tidyverse", "lubridate"), require, character.only = TRUE)
 
+# ================================================
+
 # read drugs
 drugs <- read_csv("drugs.csv")
 groups <- read_csv("groups.csv")
@@ -81,3 +83,151 @@ drugs %>% select(name, targets_count) %>% top_n(10) %>% arrange(desc(targets_cou
 
 # affected organizms
 drugs %>% select(name, affected_organisms_count) %>% top_n(10) %>% arrange(desc(affected_organisms_count))
+
+# ================================================
+
+drugs <- read_csv("drugs.csv")
+groups <- read_csv("groups.csv")
+targets <- read_csv('targets.csv')
+
+## get all actual groups
+get_all_actual_groups <- function(x) {
+    ## apply the 'get_actual_group' function to each drug
+    x <- sapply(x, get_actual_group)
+    
+    ## return the actual groups for all drugs
+    x
+}
+
+## get actual group for a single drug
+get_actual_group <- function(x) {
+    ## withdrawn > approved > investigational > experimental > other
+    if (grepl('withdrawn', x)) {               actual_group <- 'withdrawn'
+    } else if (grepl('approved', x)) {         actual_group <- 'approved'
+    } else if (grepl('investigational', x)) {  actual_group <- 'investigational'
+    } else if (grepl('experimental', x)) {     actual_group <- 'experimental'
+    } else {                                   actual_group <- 'other'}
+    
+    ## return actual final tag for the drug
+    actual_group
+}
+
+drugbank <- 
+    drugs %>% 
+    select(primary_key, created, type) %>% 
+    rename(parent_key = primary_key,
+           creationDate = created)
+
+groups <- 
+    groups %>% 
+    group_by(parent_key) %>%
+    summarise(allgroups = paste(text, collapse=';')) %>%
+    mutate(final_group = get_all_actual_groups(allgroups)) %>% 
+    select(-allgroups) %>% 
+    rename(status = final_group)
+
+targets <- 
+    targets %>%
+    group_by(parent_key) %>%
+    summarise(targets = paste(id,collapse = ';'))
+    
+drugbank <- 
+    drugbank %>% 
+    left_join(groups, by = 'parent_key') %>% 
+    left_join(targets, by = 'parent_key') %>% 
+    rename(drug = parent_key) %>% 
+    select(drug,creationDate,type, status, targets)
+
+write_csv(drugbank, 'drugbank.csv')
+
+# ================================================
+
+## plot actual groups of the drugs
+groups %>% 
+group_by(status) %>% 
+summarize(count = n()) %>%
+ggplot(aes(x = reorder(status,count), 
+           y = count, 
+           fill = letters[1:5])) +    ## alternative: fill = count
+geom_bar(stat = "identity") + 
+guides(fill=FALSE) +    ## removes legend for the bar colors
+coord_flip()            ## switches the X and Y axes
+
+# ================================================
+
+## get status Data
+statusData <- 
+    drugbank %>% 
+    mutate(creationDate = year(creationDate)) %>% 
+    group_by(creationDate,status) %>% 
+    summarise(count = n()) %>% 
+    spread(status,count) %>% 
+    ungroup()    # avoids trouble later when plotting using plotly
+
+# remove any NA values that resulted from the spread() operation
+statusData[is.na(statusData)] <- 0
+
+### get plot of the quantities of the different drug statuses over time
+#p2 <- 
+#    statusData %>% 
+#    plot_ly(  x = ~creationDate, y = ~cumsum(experimental),    type = 'scatter', mode = 'lines', name = 'experimental', height = 700) %>% 
+#    add_trace(x = ~creationDate, y = ~cumsum(investigational), type = 'scatter', mode = 'lines', name = 'investigational') %>% 
+#    add_trace(x = ~creationDate, y = ~cumsum(approved),        type = 'scatter', mode = 'lines', name = 'approved') %>% 
+#    add_trace(x = ~creationDate, y = ~cumsum(withdrawn),       type = 'scatter', mode = 'lines', name = 'withdrawn') %>% 
+#    add_trace(x = ~creationDate, y = ~cumsum(other),           type = 'scatter', mode = 'lines', name = 'other') %>% 
+#    layout(title = 'Drug Status',  xaxis = list(title='Time'), yaxis = list(title='Quantity'))
+
+### display plot
+#embed_notebook(p2)
+
+statusData %>% 
+ggplot(aes(x = creationDate)) + 
+geom_line(aes(y = cumsum(approved), col = 'approved')) + 
+geom_line(aes(y = cumsum(experimental), col = 'experimental')) + 
+geom_line(aes(y = cumsum(investigational), col = 'investigational')) + 
+geom_line(aes(y = cumsum(withdrawn), col = 'withdrawn')) + 
+geom_line(aes(y = cumsum(other), col = 'other')) + 
+#scale_y_continuous(limits = c(0,12000)) + 
+xlab('Time') + 
+ylab('Quantity') + 
+labs(title = 'Drug Status',
+     subtitle = 'Proportions of the different status values over time', 
+     caption = 'created by ggplot')
+
+# ================================================
+
+## cumulative bar chart (unfinished)
+drugs %>% 
+ggplot(aes(x = factor(year(created)), y = cumsum(..count..))) +
+geom_bar()
+
+# ================================================
+
+## get status Data
+statusData <- 
+    drugbank %>% 
+    mutate(creationDate = year(creationDate)) %>% 
+    group_by(creationDate, status) %>% 
+    summarise(count = n()) %>% 
+    spread(status, count) %>% 
+    ungroup()    # avoids trouble later when plotting using plotly
+
+# remove any NA values that resulted from the spread() operation
+statusData[is.na(statusData)] <- 0
+
+## get plot of the quantities of the different drug statuses over time
+p2 <- 
+    statusData %>% 
+    plot_ly(  x = ~creationDate, y = ~cumsum(experimental),    type = 'scatter', mode = 'lines', name = 'experimental') %>% 
+    add_trace(x = ~creationDate, y = ~cumsum(investigational), type = 'scatter', mode = 'lines', name = 'investigational') %>% 
+    add_trace(x = ~creationDate, y = ~cumsum(approved),        type = 'scatter', mode = 'lines', name = 'approved') %>% 
+    add_trace(x = ~creationDate, y = ~cumsum(withdrawn),       type = 'scatter', mode = 'lines', name = 'withdrawn') %>% 
+    add_trace(x = ~creationDate, y = ~cumsum(other),           type = 'scatter', mode = 'lines', name = 'other') %>% 
+    layout(title = 'Drug Status',  xaxis = list(title='Time'), yaxis = list(title='Quantity'))
+
+## display plot
+p2
+
+# NOTE: use embed_notebook(p2) if displaying in a jupyter notebook
+
+# ================================================
